238d466f244a74db90bc97dcf18339bf2ccf6da5
ashwinashok9111993 <ashwinashok9111993@gmail.com>
2025-07-15 15:45:45 +0530

    Refactor battery validation logic into separate methods for clarity and maintainability

--- checker.cs
+++ checker.cs
@@ -1,21 +1,18 @@
 using System;
 using System.Diagnostics;
+
 namespace paradigm_shift_csharp
 {
 class BatteryChecker
 {
-    static bool batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            Console.WriteLine("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            Console.WriteLine("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            Console.WriteLine("Charge Rate is out of range!");
-            return false;
-        }
-        return true;
+    //lambda
+    static bool IsTemperatureOk(float temperature) => temperature >= 0 && temperature <= 45;
+    static bool IsSocOk(float soc) => soc >= 20 && soc <= 80;
+    static bool IsChargeRateOk(float chargeRate) => chargeRate <= 0.8;
+
+    static bool batteryIsOk(float temperature, float soc, float chargeRate) 
+    {
+      return (IsTemperatureOk(temperature) && IsSocOk(soc) && IsChargeRateOk(chargeRate));
     }
 
     static void ExpectTrue(bool expression) {
@@ -30,6 +27,7 @@ class BatteryChecker
             Environment.Exit(1);
         }
     }
+    
     static int Main() {
         ExpectTrue(batteryIsOk(25, 70, 0.7f));
         ExpectFalse(batteryIsOk(50, 85, 0.0f));
